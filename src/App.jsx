a22bb179607afa5dcf27d26282738298cd6f18a2
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2024-03-07 14:33:24 +0530

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import Page from "./Page";
 
 function App() {
   return (
     <div className="App">
-      // Add code here....
+      <Page />
     </div>
   );
 }
